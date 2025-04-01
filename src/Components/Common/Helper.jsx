55337e59824904845d2b1d9610437da1b3d0018e
rakeshascoder <rakeshsheoran1202@gmail.com>
2025-04-01 22:03:28 +0530

    try to fix build error

--- src/Components/Common/Helper.jsx
+++ src/Components/Common/Helper.jsx
@@ -1,7 +1,7 @@
-import img1 from "../../assets/images/Png/age_selection_img1.png"
-import img2 from "../../assets/images/Png/age_selection_img2.png"
-import img3 from "../../assets/images/Png/age_selection_img3.png"
-import img4 from "../../assets/images/Png/age_selection_img4.png"
+import img1 from "../../assets/Images/Png/age_selection_img1.png"
+import img2 from "../../assets/Images/Png/age_selection_img2.png"
+import img3 from "../../assets/Images/Png/age_selection_img3.png"
+import img4 from "../../assets/Images/Png/age_selection_img4.png"
 export const AgeScreenData = [
   { id: 1, src: img1, alt: "Person 1" },
   { id: 2, src: img2, alt: "Person 2" },
